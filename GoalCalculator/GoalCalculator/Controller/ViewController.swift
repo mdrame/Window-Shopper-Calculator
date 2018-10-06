a5daab530dc6cca06db08ecf0d3d3b3351d63de5
Mohammed Drame <mohammeddrame@MohammedsiMac2.home>
2018-10-06 02:42:00 -0400

    first commit / 0 % on project

---
+++ GoalCalculator/GoalCalculator/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoalCalculator
+//
+//  Created by Mohammed Drame on 10/6/18.
+//  Copyright © 2018 Mohammed Drame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
